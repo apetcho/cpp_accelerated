5beeaf47c2e1049d3ed303e6ae83e53cd43b00ff
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-27 20:51:29 +0100

    Complete the 'frame' program of chapter 01

---
+++ src/ch01/frame.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file frame.cpp
+ * @brief Ask for a person's name, and generate a framed greeting
+ * @version 0.1
+ * @date 2021-12-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<iostream>
+#include<string>
+
+
+int main(){
+    std::cout << "Please enter your first name: ";
+    std::string name;
+    std::cin >> name;
+
+    /* build the message that we intend to write */
+    const std::string greeting = "Hello, " + name + "!";
+
+    /* build the second and fourth lines of the output */
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+
+    /* build the first and fifth lines of the output */
+    const std::string first(second.size(), '*');
+
+    /* write it all */
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
+    return 0;
+}
