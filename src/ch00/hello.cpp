cd244be48e1fc9d98188324911efc6a4aeb00e67
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-27 20:27:34 +0100

    Complete the hello program of chapter 00

---
+++ src/ch00/hello.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file hello.cpp
+ * @brief A small C++ program for printing "Hello, World!" in console.
+ * @version 0.1
+ * @date 2021-12-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<iostream>
+
+int main(){
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+// ---- Concepts introduced in chapter 00 ----
+//
+// Program structure
+//
+// Comments
+// ``// This is a comment``
+//
+// #include directives
+//
+// main function
+// 
+// Types:
+// 
+// Namespaces (std in particular)
+//
+// string literals
+// 
+// special characters in string literals including \n, \t, \b, \", \', \\ 
