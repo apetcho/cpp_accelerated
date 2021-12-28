4e9a122f79cd17f0922aae9a55d1dae37539f67c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-28 11:14:10 +0100

    Define StudentInfo data structure

---
+++ src/ch04/student_info.hpp
@@ -0,0 +1,19 @@
+#ifndef _STUDENT_INFO_H
+#define _STUDENT_INFO_H
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+struct StudentInfo{
+    std::string name;
+    double midterm;
+    double final;
+    std::vector<double> homework;
+};
+
+bool compare_student_info(const StudentInfo&, const StudentInfo&);
+std::istream& read_student_info(std::istream&, StudentInfo&);
+std::istream& read_homework(std::iostream&, std::vector<double>&)
+
+#endif
