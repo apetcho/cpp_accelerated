90498cf72353582cbac07066335616646fa15f91
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-28 23:43:05 +0100

    Implement splitMain for testing split function of ch05

---
+++ src/ch05/splitMain.cpp
@@ -0,0 +1,23 @@
+#include<cctype>
+#include<iostream>
+#include<string>
+#include<vector>
+
+#include "split.hpp"
+
+// --- MAIN DRIVER ---
+int main(){
+    std::string text;
+
+    // read and split each line of input
+    //using tsize_type = std::vector<std::string>::size_type;
+    while(std::getline(std::cin, text)){
+        std::vector<std::string> vtext = split(text);
+
+        for(auto item: vtext){
+            std::cout << item << std::endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
