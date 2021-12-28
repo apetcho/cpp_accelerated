d8268fd63d97ab0ca7bac43c0931afd3483d1b76
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-28 22:19:50 +0100

    Define overloaded version of extract_fails() function

--- src/ch05/extractFails.hpp
+++ src/ch05/extractFails.hpp
@@ -2,8 +2,10 @@
 #define _EXTRACT_FAILS_H
 
 #include<vector>
+#include<list>
 #include "../ch04/student_info.hpp"
 
 std::vector<StudentInfo> extract_fails(std::vector<StudentInfo>& vec);
+std::list<StudentInfo> extract_fails(std::list<StudentInfo>& students);
 
 #endif
