6d9048a061ddce7a8e52858d0b2cdb50dfa64052
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-28 11:52:49 +0100

    Implement grade() functions

---
+++ src/ch04/grade.cpp
@@ -0,0 +1,28 @@
+#include<stdexcept>
+#include<vector>
+
+#include "grade.hpp"
+#include "median.hpp"
+#include "student_info.hpp"
+
+/** Compute a student's overall finall grade from median and final exam grades
+ * and homework grade */
+double grade(double midterm, double final, double homework){
+    return 0.2*midterm + 0.4*final + 0.4*homework;
+}
+
+/** Compute a student grade's overall grade from midterm and final exam grades
+ * and vector of home work grades.
+ * 
+ * This function does not copy its arguments, because `median' does so for us.*/
+double grade(double midterm, double final, const std::vector<double>& homework){
+    if(homework.size() == 0){
+        throw std::domain_error("student has done no homework");
+    }
+    return grade(midterm, final, median(homework));
+}
+
+/***/
+double grade(const StudentInfo& info){
+    return grade(info.midterm, info.final, info.homework);
+}
